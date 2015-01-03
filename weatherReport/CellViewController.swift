a33dd543f58fefe9e7884fdaadfb4a4eee93af1a
tatsuya-yokoyama <yokoyama.tatsuya1984@gmail.com>
2015-01-03 13:15:06 +0900

    Added table

---
+++ weatherReport/CellViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CellViewController.swift
+//  weatherReport
+//
+//  Created by yokoyama.tatsuya on 2015/01/03.
+//  Copyright (c) 2015年 yokoyama.tatsuya. All rights reserved.
+//
+
+import UIKit
+
+class CellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
